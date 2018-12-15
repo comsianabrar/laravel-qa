a17699d64f22ba09270de7018d87ecb20188e128
Abrar Ahmed <comsianabrar@gmail.com>
2018-12-15 13:41:36 +0500

    question form, validating question and saving

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -5,9 +5,23 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header" style="font-size: x-large">Latest Questions</div>
+                    <div class="card-header">
+                        <div class="d-flex align-items-center">
+                            <h1>Latest Questions</h1>
+
+                            <div class="ml-auto">
+                                <a href="{{route('questions.create')}}" class="btn btn-outline-secondary">
+                                    Post New Question
+                                </a>
+                            </div>
+                        </div>
+
+                    </div>
 
                     <div class="card-body">
+
+                        @include('layouts._messages')
+
                         @foreach($questions AS $question)
                             <div class="media">
                                 <div class="d-flex flex-column counters">
